b1d61e19dccbb8cd788c75820fbe2aa198718444
Error-4444 <aa0938396575@gmail.com>
2024-01-06 02:51:21 +0800

    頁尾列元件建立

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent],
+  imports: [HeaderComponent, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
