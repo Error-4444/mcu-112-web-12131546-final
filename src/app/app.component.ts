bbabeb47467379c9041fe0a214add8f2fc95c94b
Error-4444 <aa0938396575@gmail.com>
2024-01-07 02:31:10 +0800

    將header資料從app移動至home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent, FooterComponent],
+  imports: [
+    NavBarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
