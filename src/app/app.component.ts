8ebb22dfe6c411148cc903fa50eaaff4574b9dd1
Error-4444 <aa0938396575@gmail.com>
2024-01-07 01:04:43 +0800

    登入與註冊元件建立

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
