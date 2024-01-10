8d8f072671a03a23839ad05592ab9c6041d82253
Error-4444 <aa0938396575@gmail.com>
2024-01-10 23:19:54 +0800

    feat(tag):標籤元件樣式實作

--- src/app/tags-list/tags-list.component.ts
+++ src/app/tags-list/tags-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, HostBinding } from '@angular/core';
+import { TagComponent } from '../tag/tag.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tags-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, TagComponent],
   templateUrl: './tags-list.component.html',
   styleUrl: './tags-list.component.css',
 })
 export class TagsListComponent {
-  @HostBinding('class') class = 'tag-list';
+  @HostBinding('class') class = 'tags-list';
+  tags = ['enim', 'repellat', 'est', 'eos'];
 }
