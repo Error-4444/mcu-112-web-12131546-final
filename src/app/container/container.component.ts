e24808fe4879842fe2adf148632d3914eda4e813
Error-4444 <aa0938396575@gmail.com>
2024-01-11 00:36:27 +0800

    feat(article):文章預覽元件與實作

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TagsListComponent } from '../tags-list/tags-list.component';
+import { ArticleListComponent } from '../article-list/article-list.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [TagsListComponent],
+  imports: [TagsListComponent, ArticleListComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
