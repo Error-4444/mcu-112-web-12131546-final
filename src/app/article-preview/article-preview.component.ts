7f3082ffeadb82d4a4b963ed9d584fe8dda596ac
Error-4444 <aa0938396575@gmail.com>
2024-01-11 22:19:17 +0800

    feat(model):文章資料類別定義新增

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,17 +1,36 @@
-import { NgFor } from '@angular/common';
+import { DatePipe, NgFor } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
 import { TagComponent } from '../tag/tag.component';
 import { TagsListComponent } from '../tags-list/tags-list.component';
 import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { Article } from '../model/article';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [TagsListComponent, FavoriteButtonComponent],
+  imports: [TagsListComponent, FavoriteButtonComponent, DatePipe],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
   @HostBinding('class') class = 'article-preview';
-  tags = ['enim', 'repellat', 'est', 'eos'];
+
+  article = new Article({
+    id: 1,
+
+    title:
+      'Ill quantify the redundant TCP bus,that should hard drive the ADP bandwidth!',
+
+    content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
+    debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
+    corporis corporis possimus.`,
+
+    favoriteCount: 30,
+
+    author: 'Error-4444',
+
+    createDate: new Date(2024, 6, 4),
+
+    tags: ['enim', 'repellat', 'est', 'eos'],
+  });
 }
