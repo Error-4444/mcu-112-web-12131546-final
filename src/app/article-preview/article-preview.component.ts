f63215e1da8d66a697341d99a1e2dc2f509189f2
Error-4444 <aa0938396575@gmail.com>
2024-01-11 23:15:27 +0800

    feat(article):變更首頁與文章清單元件顯示實作

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -5,33 +5,54 @@ import { TagsListComponent } from '../tags-list/tags-list.component';
 import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
 import { Article } from '../model/article';
 import { ArticleComponent } from '../article/article.component';
+import { ArticleListComponent } from '../article-list/article-list.component';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [ArticleComponent],
+  imports: [ArticleListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
   @HostBinding('class') class = 'article-preview';
 
-  article = new Article({
-    id: 1,
+  articles = [
+    new Article({
+      id: 1,
 
-    title:
-      'Ill quantify the redundant TCP bus,that should hard drive the ADP bandwidth!',
+      title:
+        'Ill quantify the redundant TCP bus,that should hard drive the ADP bandwidth!',
 
-    content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
+      content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
     debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
     corporis corporis possimus.`,
 
-    favoriteCount: 30,
+      favoriteCount: 30,
 
-    author: 'Error-4444',
+      author: 'Error-4444',
 
-    createDate: new Date(2024, 1, 11),
+      createDate: new Date(2024, 1, 11),
 
-    tags: ['enim', 'repellat', 'est', 'eos'],
-  });
+      tags: ['enim', 'repellat', 'est', 'eos'],
+    }),
+    new Article({
+      id: 2,
+
+      title:
+        'Ill quantify the redundant TCP bus,that should hard drive the ADP bandwidth!',
+
+      content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
+    debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
+    corporis corporis possimus.`,
+
+      favoriteCount: 30,
+
+      author: 'Error-4444',
+
+      createDate: new Date(2024, 1, 11),
+
+      tags: ['enim', 'repellat', 'est', 'eos'],
+    }),
+  ];
 }
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -3,11 +3,12 @@ import { TagsListComponent } from '../tags-list/tags-list.component';
 import { ArticleListComponent } from '../article-list/article-list.component';
 import { TagSidebarComponent } from '../tag-sidebar/tag-sidebar.component';
 import { ArticleTabComponent } from '../article-tab/article-tab.component';
+import { ArticlePreviewComponent } from '../article-preview/article-preview.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [ArticleTabComponent, ArticleListComponent, TagSidebarComponent],
+  imports: [ArticleTabComponent, ArticlePreviewComponent, TagSidebarComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
