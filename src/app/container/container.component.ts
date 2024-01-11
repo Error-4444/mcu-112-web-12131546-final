890e02e750e04ab8e63e81cb43b3ebe46f7577de
Error-4444 <aa0938396575@gmail.com>
2024-01-11 18:08:21 +0800

    feat(tab):文章頁籤元件實作

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TagsListComponent } from '../tags-list/tags-list.component';
 import { ArticleListComponent } from '../article-list/article-list.component';
 import { TagSidebarComponent } from '../tag-sidebar/tag-sidebar.component';
+import { ArticleTabComponent } from '../article-tab/article-tab.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [ArticleListComponent, TagSidebarComponent],
+  imports: [ArticleTabComponent, ArticleListComponent, TagSidebarComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
